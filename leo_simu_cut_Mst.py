9ea46b0f3692fd485e591a79394f6557d3c0a2a3
Antonela Taverna <70217832+antotaverna@users.noreply.github.com>
2023-09-22 01:12:54 -0300

    Update leo_simu_cut_Mst.py

--- leo_simu_cut_Mst.py
+++ leo_simu_cut_Mst.py
@@ -7,19 +7,17 @@ basePath = '/big4/simul/Illustris-TNG300/output'
 fields = ['SubhaloFlag',
           'SubhaloGrNr',
           'SubhaloCM',
-          'SubhaloMass',
-          'SubhaloHalfmassRad',
-          'SubhaloHalfmassRadType',
-          'SubhaloParent',
           'SubhaloPos',
           'SubhaloVel',
           'SubhaloVelDisp',
           'SubhaloSFR',
           'SubhaloStellarPhotometrics',
+          'SubhaloHalfmassRad',
+          'SubhaloMass',
           'SubhaloStellarPhotometricsMassInRad',
           'SubhaloStellarPhotometricsRad',
           'SubhaloMassInRadType',
-          ]
+          'SubhaloHalfmassRadType']
 
 subhalos = il.groupcat.loadSubhalos(basePath,99,fields=fields)
 print('subhalos keys:',subhalos.keys())
@@ -33,10 +31,10 @@ print('subhalos.shape=',subhalos['SubhaloMass'].shape)
 fields_h = ['GroupFirstSub',
             'GroupNsubs',
             'GroupCM',
-            'Group_M_Crit200',
-            'Group_M_Mean200',
             'GroupPos',
             'GroupVel',
+            'Group_M_Crit200',
+            'Group_M_Mean200',
             'Group_R_Crit200',
             'Group_R_Mean200']
 
@@ -51,6 +49,8 @@ mag = subhalos['SubhaloStellarPhotometrics']
 #link --> GroupFirstSub(halo) + SubhaloGrNr(sub halo)
 f = open ("galaxycatalogue_cut_Mst.dat", "w")
 #for i in range(len(first[:2])):
+
+#El for recorre cada halo y va escribiendo las propiedades de sus subhalos.
 for i in range(len(first)):
     if(i%10000==0):
         print('****', i)
@@ -60,19 +60,47 @@ for i in range(len(first)):
     ii=np.where((subhalos['SubhaloGrNr']==i) & (subhalos['SubhaloStellarPhotometricsMassInRad']>0.001))
     for j in range(len(subhalos['SubhaloCM'][ii])):
 
-        cadena=str(subhalos['SubhaloCM'][ii][j][0])+'  '+str(subhalos['SubhaloCM'][ii][j][1])+'  '+str(subhalos['SubhaloCM'][ii][j][2])+\
-            '  '+str(subhalos['SubhaloHalfmassRad'][ii][j])+'  '+str(subhalos['SubhaloMass'][ii][j])+\
-            '  '+str(subhalos['SubhaloStellarPhotometricsMassInRad'][ii][j])+'  '+str(subhalos['SubhaloStellarPhotometricsRad'][ii][j])+\
-            '  '+str(subhalos['SubhaloParent'][ii][j])+'  '+str(subhalos['SubhaloGrNr'][ii][j])+\
-            '  '+str(subhalos['SubhaloPos'][ii][j][0])+'  '+str(subhalos['SubhaloPos'][ii][j][1])+'  '+str(subhalos['SubhaloPos'][ii][j][2])+\
-            '  '+str(subhalos['SubhaloSFR'][ii][j])+'  '+str(subhalos['SubhaloVelDisp'][ii][j])+'  '+str(subhalos['SubhaloVel'][ii][j][0])+\
-            '  '+str(subhalos['SubhaloVel'][ii][j][1])+'  '+str(subhalos['SubhaloVel'][ii][j][2])+\
-            '  '+str(mag[ii][j][0])+'  '+str(mag[ii][j][1])+'  '+str(mag[ii][j][2])+'  '+str(mag[ii][j][3])+\
-            '  '+str(mag[ii][j][4])+'  '+str(mag[ii][j][5])+'  '+str(mag[ii][j][6])+'  '+str(mag[ii][j][7])+\
-            '  '+str(first[i])+'  '+str(n_subs[i])+'  '+str(halos['Group_M_Crit200'][i])+'  '+str(halos['Group_M_Mean200'][i])+\
-            '  '+str(halos['Group_R_Crit200'][i])+'  '+str(halos['Group_R_Mean200'][i])+\
-            '  '+str(halos['GroupCM'][i][0])+'  '+str(halos['GroupCM'][i][1])+'  '+str(halos['GroupCM'][i][2])+\
-            '  '+str(halos['GroupPos'][i][0])+'  '+str(halos['GroupPos'][i][1])+'  '+str(halos['GroupPos'][i][2])+\
-            '  '+str(halos['GroupVel'][i][0])+'  '+str(halos['GroupVel'][i][1])+'  '+str(halos['GroupVel'][i][2])+'\n'
+        cadena=str(subhalos['SubhaloGrNr'][ii][j])+\
+              '  '+str(subhalos['SubhaloCM'][ii][j][0])+\
+              '  '+str(subhalos['SubhaloCM'][ii][j][1])+\
+              '  '+str(subhalos['SubhaloCM'][ii][j][2])+\
+              '  '+str(subhalos['SubhaloPos'][ii][j][0])+\
+              '  '+str(subhalos['SubhaloPos'][ii][j][1])+\
+              '  '+str(subhalos['SubhaloPos'][ii][j][2])+\
+              '  '+str(subhalos['SubhaloVel'][ii][j][0])+\
+              '  '+str(subhalos['SubhaloVel'][ii][j][1])+\
+              '  '+str(subhalos['SubhaloVel'][ii][j][2])+\
+              '  '+str(subhalos['SubhaloVelDisp'][ii][j])+\
+              '  '+str(subhalos['SubhaloSFR'][ii][j])+\
+              '  '+str(mag[ii][j][0])+\
+              '  '+str(mag[ii][j][1])+\
+              '  '+str(mag[ii][j][2])+\
+              '  '+str(mag[ii][j][3])+\
+              '  '+str(mag[ii][j][4])+\
+              '  '+str(mag[ii][j][5])+\
+              '  '+str(mag[ii][j][6])+\
+              '  '+str(mag[ii][j][7])+\
+              #'  '+str(subhalos['SubhaloHalfmassRad'][ii][j])+\
+              #'  '+str(subhalos['SubhaloMass'][ii][j])+\
+              #'  '+str(subhalos['SubhaloStellarPhotometricsMassInRad'][ii][j])+\
+              #'  '+str(subhalos['SubhaloStellarPhotometricsRad'][ii][j])+\
+              '  '+str(subhalos['SubhaloMassInRadType'][ii][j][4])+\   #Type=4 Stars --> stellar mass
+              '  '+str(subhalos['SubhaloHalfmassRadType'][ii][j][4])+\ #Type=4 Stars --> radio de luz(blending)
+              '  '+str(first[i])+\
+              '  '+str(n_subs[i])+\
+              '  '+str(halos['Group_M_Crit200'][i])+\
+              '  '+str(halos['Group_M_Mean200'][i])+\
+              '  '+str(halos['Group_R_Crit200'][i])+\
+              '  '+str(halos['Group_R_Mean200'][i])+\
+              '  '+str(halos['GroupCM'][i][0])+\
+              '  '+str(halos['GroupCM'][i][1])+\
+              '  '+str(halos['GroupCM'][i][2])+\
+              '  '+str(halos['GroupPos'][i][0])+\
+              '  '+str(halos['GroupPos'][i][1])+\
+              '  '+str(halos['GroupPos'][i][2])+\
+              '  '+str(halos['GroupVel'][i][0])+\
+              '  '+str(halos['GroupVel'][i][1])+\
+              '  '+str(halos['GroupVel'][i][2])+\
+              '\n'
         f.write(cadena)
 f.close()
